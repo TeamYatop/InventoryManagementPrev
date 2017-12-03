bcb0bf47a0e1eb110fe3313dea84f46f0592f93c
sshong1991 <sshong1991@gmail.com>
2017-12-03 15:47:56 +0900

    app rename & add requirments.txt

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'inventory_management.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'inventory_management.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
